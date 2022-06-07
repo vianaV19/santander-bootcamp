208ac00e9705718c6819d622c5a28ee89e9c608f
Matheus Viana de Souza <matheus.viana518062@gmail.com>
2022-06-07 10:15:50 -0300

    Delete Javascript directory

--- Javascript/palindromo.js
+++
@@ -1,7 +0,0 @@
-function verificaPalindromo(string){
-    if(!string) return;
-
-    return string.split('').reverse().join('') == string ? true : false;
-}
-
-console.log(verificaPalindromo('arara'))
